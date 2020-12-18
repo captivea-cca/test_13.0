17f05710312c393d2af392d55847a27fde4a82b2
Chandler Calderon <72744609+captivea-cca@users.noreply.github.com>
2020-12-17 22:05:29 -0500

    Update controllers.py

--- cap_customer_pricing/controllers/controllers.py
+++ cap_customer_pricing/controllers/controllers.py
@@ -13,27 +13,27 @@ class ClientPricingWeb(http.Controller):
             'modules': Modules.sudo().search([])
         })
 
-    @http.route('/customer_pricing/submitted', type="http", auth='public', website=True)
-    def pricing_submitted(self, **kw):
-        if int(kw['num_users']) > 50:
+    @http.route('/customer_pricing/submitted', type="http", auth='public', methods=['POST'] website=True)
+    def pricing_submitted(self, **kwargs):
+        if int(kwargs['num_users']) > 50:
             return request.redirect('/customer_pricing/contact_us')
 
         pricing = {
-            'name': kw['company_name'],
-            'company_name': kw['company_name'],
-            'phone_number': kw['phone_number'],
-            'email': kw['email'],
-            'num_users': kw['num_users'],
+            'name': kwargs['company_name'],
+            'company_name': kwargs['company_name'],
+            'phone_number': kwargs['phone_number'],
+            'email': kwargs['email'],
+            'num_users': kwargs['num_users'],
         }
 
         pricing_saved = request.env['cap_customer_pricing.pricing'].sudo(
                 ).create(pricing)
 
-        for value in kw:
+        for value in kwargs:
             if value.startswith('module'):
                 estimate_line = {
                     'estimate_id': pricing_saved.id,
-                    'module_id': kw[value],
+                    'module_id': kwargs[value],
                 }
                 request.env['cap_customer_pricing.line'].sudo().create(
                     estimate_line)
